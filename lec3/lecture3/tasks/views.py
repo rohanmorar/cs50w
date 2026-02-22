1c539abd10cd6f1f1f7fce36fa152feef6d1c3cd
futurerohan <195040750+futurerohan@users.noreply.github.com>
2026-02-22 17:35:18 -0600

    added django form with client/server side validation and redirect

--- lec3/lecture3/tasks/views.py
+++ lec3/lecture3/tasks/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django import forms
+from django.urls import reverse
 
-tasks = ["foo", "baz", "bar"]
+tasks = []
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label = "New Task")
+    # priority = forms.IntegerField(label = "Priority", min_value=1, max_value=5)s
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {
@@ -8,4 +15,16 @@ def index(request):
     })
 
 def add(request): 
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
